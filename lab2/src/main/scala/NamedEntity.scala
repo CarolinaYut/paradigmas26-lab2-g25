4763aa70ef9a5c3c95a513ceb5286e6df5fc2f0f
Carolina Yut <carolina.yut@mi.unc.edu.ar>
2026-04-25 22:12:02 -0300

    avances ejercicio 1

--- lab2/src/main/scala/NamedEntity.scala
+++ lab2/src/main/scala/NamedEntity.scala
@@ -29,6 +29,30 @@ abstract class NamedEntity(val text: String) {
   def describe: String = s"[$entityType] $text"
 }
 
+class Person extends NamedEntity (val text: String) {
+  def entityType = "Person"
+} 
+
+class Organization extends NamedEntity (val text:String){
+  def entityType = "Organization"
+}
+
+class University extends Organization (val text:String){
+  override def entityType = "University"
+}
+
+class Place extends NamedEntity (val text:String){
+  def entityType = "Place"
+}
+
+class Technology extends NamedEntity (val text:String){
+  def entityType = "Technology"
+}
+
+class ProgrammingLanguage extends Technology (val text:String){
+  def entityType = "ProgrammingLanguage"
+}
+
 // =====================================================================
 // TODO (Ejercicio 1): Completar la jerarquía de entidades
 //
